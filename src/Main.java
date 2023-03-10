1ef743fe3cd998b7391553eb65808d81d520d7d6
evgeniy-b64 <evgeniy.b64@yandex.ru>
2023-03-10 22:14:51 +0400

    Добавлены цвета вывода для матрицы

--- src/Main.java
+++ src/Main.java
@@ -7,107 +7,109 @@ public class Main {
         task5();
     }
 
+    public static void taskHeader(int number, String caption) {
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("|| Задача " + number + ": " + caption + " ||");
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    }
+
+
     public static void task1() {
-        var separator = "=-=-=-=-=-=-=-=-=-=-=-=-=-=";
-        System.out.println(separator);
-        System.out.println("|| Задача 1: Объявление массивов ||");
-        System.out.println(separator);
+        taskHeader(1, "Объявление массивов");
         int[] arr1 = new int[]{1, 2, 3};
         float[] arr2 = {1.57f, 7.654f, 9.986f};
-        boolean[] arr3 = {true,false,false};
+        boolean[] arr3 = {true, false, false};
 
     }
 
     public static void task2() {
-        var separator = "=-=-=-=-=-=-=-=-=-=-=-=-=-=";
-        System.out.println(separator);
-        System.out.println("|| Задача 2: Вывод массивов ||");
-        System.out.println(separator);
+        taskHeader(2, "Вывод массивов");
         int[] arr1 = new int[]{1, 2, 3};
         float[] arr2 = {1.57f, 7.654f, 9.986f};
-        boolean[] arr3 = {true,false,false};
+        boolean[] arr3 = {true, false, false};
 
-        for (int i = 0; i < arr1.length-1; i++) {
-            System.out.print(arr1[i]+", ");
+        for (int i = 0; i < arr1.length - 1; i++) {
+            System.out.print(arr1[i] + ", ");
         }
-        System.out.println(arr1[arr1.length-1]);
+        System.out.println(arr1[arr1.length - 1]);
 
-        for (int i = 0; i < arr2.length-1; i++) {
-            System.out.print(arr2[i]+", ");
+        for (int i = 0; i < arr2.length - 1; i++) {
+            System.out.print(arr2[i] + ", ");
         }
-        System.out.println(arr2[arr2.length-1]);
+        System.out.println(arr2[arr2.length - 1]);
 
-        for (int i = 0; i < arr3.length-1; i++) {
-            System.out.print(arr3[i]+", ");
+        for (int i = 0; i < arr3.length - 1; i++) {
+            System.out.print(arr3[i] + ", ");
         }
-        System.out.println(arr3[arr3.length-1]);
+        System.out.println(arr3[arr3.length - 1]);
     }
 
     public static void task3() {
-        var separator = "=-=-=-=-=-=-=-=-=-=-=-=-=-=";
-        System.out.println(separator);
-        System.out.println("|| Задача 3: вовиссам довыВ ||");
-        System.out.println(separator);
+        taskHeader(3, "вовиссам довыВ");
         int[] arr1 = new int[]{1, 2, 3};
         float[] arr2 = {1.57f, 7.654f, 9.986f};
-        boolean[] arr3 = {true,false,false};
+        boolean[] arr3 = {true, false, false};
 
-        for (int i = arr1.length-1; i > 0; i--) {
-            System.out.print(arr1[i]+", ");
+        for (int i = arr1.length - 1; i > 0; i--) {
+            System.out.print(arr1[i] + ", ");
         }
         System.out.println(arr1[0]);
 
-        for (int i = arr2.length-1; i > 0; i--) {
-            System.out.print(arr2[i]+", ");
+        for (int i = arr2.length - 1; i > 0; i--) {
+            System.out.print(arr2[i] + ", ");
         }
         System.out.println(arr2[0]);
 
-        for (int i = arr3.length-1; i > 0; i--) {
-            System.out.print(arr3[i]+", ");
+        for (int i = arr3.length - 1; i > 0; i--) {
+            System.out.print(arr3[i] + ", ");
         }
         System.out.println(arr3[0]);
     }
 
     public static void task4() {
-        var separator = "=-=-=-=-=-=-=-=-=-=-=-=-=-=";
-        System.out.println(separator);
-        System.out.println("|| Задача 4: Нечётное-чётное ||");
-        System.out.println(separator);
+        taskHeader(4, "Нечётное -> чётное");
         int[] arr1 = new int[]{1, 2, 3};
 
         for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] % 2 != 0){
+            if (arr1[i] % 2 != 0) {
                 arr1[i]++;
             }
         }
-        for (int i = 0; i < arr1.length-1; i++) {
-            System.out.print(arr1[i]+", ");
+        for (int i = 0; i < arr1.length - 1; i++) {
+            System.out.print(arr1[i] + ", ");
         }
-        System.out.println(arr1[arr1.length-1]);
+        System.out.println(arr1[arr1.length - 1]);
     }
 
     public static void task5() {
-        var separator = "=-=-=-=-=-=-=-=-=-=-=-=-=-=";
-        System.out.println(separator);
-        System.out.println("|| The Matrix ||");                 //Wake up, Neo...
-        System.out.println(separator);
+
+        // пока не совсем понимаю, как это работает
+        final String ANSI_RESET = "\u001B[0m";                  //возвращает цвет текста в консоли к значению по умолчанию
+        final String ANSI_GREEN = "\u001B[32m";                 //меняет цвет текста в консоли на зелёный
+        //=========================================
+
+        taskHeader(5, "The Matrix");              //Wake up, Neo...
         byte matrixSize = 6;                                    //размер матрицы
         int[][] theMatrix = new int[matrixSize][matrixSize];    //объявление двумерного массива
         int count = 1;
         for (int j = 0; j < matrixSize; j++) {                  //проходим по рядам
-            for (int i = 0; i < matrixSize; i++){               //проходим по столбцам
+            for (int i = 0; i < matrixSize; i++) {              //проходим по столбцам
 
-                theMatrix[i][j] = count+i;
-                if (count+i == matrixSize) break;
+                theMatrix[i][j] = count + i;
+                if (count + i == matrixSize) break;
             }
             count++;
         }
         for (int j = 0; j < matrixSize; j++) {                  //проходим по рядам
-            for (int i = 0; i < matrixSize-1; i++) {            //проходим по столбцам
-                System.out.print(theMatrix[i][j] + ", ");
+            for (int i = 0; i < matrixSize - 1; i++) {          //проходим по столбцам
+                if (theMatrix[i][j] > 0) {
+                    System.out.print(ANSI_GREEN + theMatrix[i][j] + ", " + ANSI_RESET);       //вывод ненулевых элементов матрицы зелёным цветом
+                } else
+                    System.out.print(theMatrix[i][j] + ", ");                                 //вывод нулевых элементов матрицы цветом по умолчанию
             }
-            System.out.println(theMatrix[matrixSize-1][j]);
+            if (theMatrix[matrixSize - 1][j] > 0) {
+                System.out.println(ANSI_GREEN + theMatrix[matrixSize - 1][j] + ANSI_RESET);
+            } else System.out.println(theMatrix[matrixSize - 1][j]);
         }
-
     }
 }
